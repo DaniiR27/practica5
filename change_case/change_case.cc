96a3fdb73fb2cbb94d895ce663321c4b4797bb6f
Daniel Del Rosario <danidelrosario7@gmail.com>
2021-11-03 23:30:06 +0000

    change case finish

--- change_case/change_case.cc
+++ change_case/change_case.cc
@@ -6,19 +6,33 @@
  *
  * @author Daniel Del Rosario Pimienta
  * @date 30 Oct 2021
- * @brief Arithmetic operators
- * Arithmetic operators
+ * @brief change_case
+ * Change case
  *
  */
+
 #include<iostream>
 
 int main(){
-  char letra_introducida;
-  std::cin >> letra_introducida;
-  if  (letra_introducida >= 'a' && letra_introducida <= 'z')  {
-    std::cout << "Es minuscula" << std::endl;
-  } else {
-    std::cout << "Es mayuscula" << std::endl;
+  char letra_introducida{' '};
+  char changed_case{' '};
+  bool correct_entry{false};
+  std::cout << "We are going to change the letter you enter for its corresponding upper or lower case" << std::endl;
+  while (!correct_entry){ 
+    std::cout << "Enter one letter: ";
+    std::cin >> letra_introducida;
+    if (isalpha(letra_introducida)) {
+      correct_entry = true;
+    if  (letra_introducida >= 'a' && letra_introducida <= 'z')  {
+      letra_introducida = std::toupper(letra_introducida);
+      std::cout << "Corresponding uppercase: "<< letra_introducida << std::endl;
+    } else {
+      letra_introducida = std::tolower(letra_introducida);
+      std::cout << "Corresponding lowercase: "<< letra_introducida << std::endl;
+    }
+  }else{
+    std::cout << "Please enter a letter" << std::endl;
+  }
   }
   return 0;
 }
